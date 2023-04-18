149b6a031c36e15e279e09af486c477ec5bd9635
Isabella <97139468+Isabellaofficial@users.noreply.github.com>
2023-04-18 16:59:45 +0900

    1..

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <img id="logo" src="./assets/logo.png">
-  
 
     <div id="nav">
-      <router-link to="/mainpage">메인페이지로</router-link>
+     
+ 
+      
+      
     </div>
     <router-view />
   </div>
@@ -22,14 +23,24 @@ export default {
 
 <style>
 body {
-  background-color: lightgrey;
+  padding:0; margin:0; -ms-user-select: none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; user-select: none;
 }
+
 #app {
+  margin:0 auto; width:750px; min-height:100%; padding-top:71px; border-left:1px #D8D8D8 solid; border-right:1px #D8D8D8 solid; 
+  position: relative; 
+  text-align:center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+@media(max-width:750px){
+    #app{ width:100%; border-left:0; border-right:0; }
+  }
+
+</style>
+
+<style scoped>
+  #mappage{ width:inherit; height:600px; position:relative; }
 </style>
