0fa741f88c532779c37c8cf5ed45046e4931fc65
Jinnymoon <jinnymoon1124@gmail.com>
2023-10-12 11:50:46 +0900

    feat: mkwk 프론트 최신화

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/wk': {
+        // 백앤드 주소 작성
+        target: 'http://localhost:18080',
+        changeOrigin: true,
+        secure: false,
+        // wk로 시작하는 거에 대해서 wk지우고 
+        pathRewrite: { '^/wk': '/wk' },
+        logLevel: 'debug',
+      },
+    },
+  },
+});
